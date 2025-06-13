99beb05574b981eebcbddbf636a34b222b63d7e7
taddyb <Tadd.N.Bindas@rtx.com>
2025-06-13 10:11:02 -0400

    Running pre-commit hooks to pass linter

--- engine/adjacency.py
+++ engine/adjacency.py
@@ -4,47 +4,45 @@
 @author Nels Frazier
 @author Tadd Bindas
 
-@date Febuary 8, 2025
-@version 0.2
+@date June 12 2025
+@version 1.0
 
 An introduction script for building a lower triangular adjancency matrix
 from a NextGen hydrofabric and writing a sparse zarr group
 """
+import graphlib as gl
 from pathlib import Path
 
 import geopandas as gpd
 import numpy as np
-import graphlib as gl
-
-from scipy import sparse
-from typing import Tuple
 import zarr
+from scipy import sparse
+
 
 def index_matrix(matrix: np.ndarray, fp: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     """
     Create a 2D dataframe with ros and columns indexed by flowpath IDs
     and values from the lower triangular adjacency matrix.
-    
+
     Parameters
     ----------
     matrix : np.ndarray
         Lower triangular adjacency matrix.
     fp : gpd.GeoDataFrame
         Flowpaths dataframe with 'toid' column indicating downstream nexus IDs.
-    
+
     Returns
     -------
     gpd.GeoDataFrame
         matrix dataframe with flowpath IDs as index and columns
     """
+
     # Create a new GeoDataFrame with the same index as the flowpaths
     matrix_df = gpd.GeoDataFrame(
-        index=fp.index,
-        columns=fp.index,
-        data=np.zeros((len(fp), len(fp)), dtype=int)
+        index=fp.index, columns=fp.index, data=np.zeros((len(fp), len(fp)), dtype=int)
     )
-    matrix_df.rename_axis('to', inplace=True)
-    matrix_df.rename_axis('from', axis=1, inplace=True)
+    matrix_df.rename_axis("to", inplace=True)
+    matrix_df.rename_axis("from", axis=1, inplace=True)
     # Fill the dataframe with the values from the matrix
     for i in range(len(fp)):
         for j in range(len(fp)):
@@ -52,19 +50,23 @@ def index_matrix(matrix: np.ndarray, fp: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
 
     return matrix_df
 
+
 _tnx_counter = 0
 
-def create_matrix(fp: gpd.GeoDataFrame, network: gpd.GeoDataFrame, ghost=False) -> Tuple[np.ndarray, list[str]]:
+
+def create_matrix(
+    fp: gpd.GeoDataFrame, network: gpd.GeoDataFrame, ghost=False
+) -> tuple[np.ndarray, list[str]]:
     """
     Create a lower triangular adjacency matrix from flowpaths and network dataframes.
-    
+
     Parameters
     ----------
     fp : gpd.GeoDataFrame
         Flowpaths dataframe with 'toid' column indicating downstream nexus IDs.
     network : gpd.GeoDataFrame
         Network dataframe with 'toid' column indicating downstream flowpath IDs.
-    
+
     Returns
     -------
     np.ndarray
@@ -75,20 +77,20 @@ def create_matrix(fp: gpd.GeoDataFrame, network: gpd.GeoDataFrame, ghost=False) 
     sorter = gl.TopologicalSorter()
 
     for id in fp.index:
-        nex = fp.loc[id]['toid']
+        nex = fp.loc[id]["toid"]
         # if isinstance(nex, float) and np.isnan(nex):
         #     continue
         try:
-            ds_wb = network.loc[nex]['toid']
+            ds_wb = network.loc[nex]["toid"]
         except KeyError:
             print("Terminal nex???", nex)
             ds_wb = np.nan
         if isinstance(ds_wb, float) and np.isnan(ds_wb):
             if ghost:
                 ds_wb = f"ghost-{_tnx_counter}"
-                network.loc[nex, 'toid'] = ds_wb
-                network.loc[ds_wb, 'toid'] = np.nan
-                fp.loc[ds_wb, 'toid'] = np.nan
+                network.loc[nex, "toid"] = ds_wb
+                network.loc[ds_wb, "toid"] = np.nan
+                fp.loc[ds_wb, "toid"] = np.nan
                 _tnx_counter += 1
         if isinstance(ds_wb, gpd.pd.Series):
             ds_wb = ds_wb.iloc[0]
@@ -100,21 +102,21 @@ def create_matrix(fp: gpd.GeoDataFrame, network: gpd.GeoDataFrame, ghost=False) 
     if ghost:
         ts_order = list(sorter.static_order())
     else:
-        ts_order = list(filter(lambda s: not (isinstance(s, float) and np.isnan(s)) ,sorter.static_order()))
-    
+        ts_order = list(filter(lambda s: not (isinstance(s, float) and np.isnan(s)), sorter.static_order()))
+
     # Reindex the flowpaths based on the topo order
     fp = fp.reindex(ts_order)
 
     # Create matrix, "indexed" the same as the re-ordered fp dataframe
-    matrix = np.zeros( (len(fp), len(fp) ) )
+    matrix = np.zeros((len(fp), len(fp)))
 
     for wb in ts_order:
-        nex = fp.loc[wb]['toid']
+        nex = fp.loc[wb]["toid"]
         if isinstance(nex, float) and np.isnan(nex):
             continue
         # Use the network to find wb -> wb topology
         try:
-            ds_wb = network.loc[nex]['toid']
+            ds_wb = network.loc[nex]["toid"]
             if isinstance(ds_wb, gpd.pd.Series):
                 ds_wb = ds_wb.iloc[0]
         except KeyError:
@@ -124,8 +126,8 @@ def create_matrix(fp: gpd.GeoDataFrame, network: gpd.GeoDataFrame, ghost=False) 
             continue
         idx = fp.index.get_loc(wb)
         idxx = fp.index.get_loc(ds_wb)
-        fp['matrix_idxx'] = idxx
-        fp['matrix_idx'] = idx
+        fp["matrix_idxx"] = idxx
+        fp["matrix_idx"] = idx
         # print(wb, " -> ", nex, " -> ", ds_wb)
         # Set the matrix value
         matrix[idxx][idx] = 1
@@ -135,7 +137,10 @@ def create_matrix(fp: gpd.GeoDataFrame, network: gpd.GeoDataFrame, ghost=False) 
     _tnx_counter = 0
     return matrix, ts_order
 
-def matrix_to_zarr(matrix: np.ndarray, ts_order: list[str], name: str, out_path: Path | str | None = None) -> None:
+
+def matrix_to_zarr(
+    matrix: np.ndarray, ts_order: list[str], name: str, out_path: Path | str | None = None
+) -> None:
     """
     Convert a lower triangular adjacency matrix to a sparse COO matrix and save it in a zarr group.
 
@@ -149,6 +154,7 @@ def matrix_to_zarr(matrix: np.ndarray, ts_order: list[str], name: str, out_path:
         Name of the zarr group to create.
     out_path : Path | str | None, optional
         Path to save the zarr group. If None, defaults to current working directory with name appended.
+
     Returns
     -------
     None
@@ -157,26 +163,18 @@ def matrix_to_zarr(matrix: np.ndarray, ts_order: list[str], name: str, out_path:
     out_path = Path(out_path) if out_path is not None else Path.cwd() / f"{name}_adjacency.zarr"
     store = zarr.storage.LocalStore(root=out_path)
     if out_path.exists():
-        root = zarr.open_group(store=store) 
+        root = zarr.open_group(store=store)
     else:
-        root = zarr.create_group(store=store)   
+        root = zarr.create_group(store=store)
 
     coo = sparse.coo_matrix(matrix)
     zarr_order = np.array([int(_id.split("-")[1]) for _id in ts_order], dtype=np.int32)
 
     gauge_root = root.create_group(name=name)
-    indices_0 = gauge_root.create_array(
-        name='indices_0', shape=coo.row.shape, dtype=coo.row.dtype
-    )
-    indices_1 = gauge_root.create_array(
-        name='indices_1', shape=coo.col.shape, dtype=coo.row.dtype
-    )
-    values = gauge_root.create_array(
-        name='values', shape=coo.data.shape, dtype=coo.data.dtype
-    )
-    order = gauge_root.create_array(
-        name='order', shape=zarr_order.shape, dtype=zarr_order.dtype
-    )
+    indices_0 = gauge_root.create_array(name="indices_0", shape=coo.row.shape, dtype=coo.row.dtype)
+    indices_1 = gauge_root.create_array(name="indices_1", shape=coo.col.shape, dtype=coo.row.dtype)
+    values = gauge_root.create_array(name="values", shape=coo.data.shape, dtype=coo.data.dtype)
+    order = gauge_root.create_array(name="order", shape=zarr_order.shape, dtype=zarr_order.dtype)
     indices_0[:] = coo.row
     indices_1[:] = coo.col
     values[:] = coo.data
@@ -190,6 +188,7 @@ def matrix_to_zarr(matrix: np.ndarray, ts_order: list[str], name: str, out_path:
         "values": coo.data.dtype.__str__(),
     }
     print(f"{name} written to zarr at {out_path}")
+
 
 if __name__ == "__main__":
     import argparse
@@ -218,9 +217,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Useful for some debugging, not needed for algorithm
-    # nexi = gpd.read_file(pkg, layer='nexus').set_index('id') 
-    fp = gpd.read_file(args.pkg, layer='flowpaths').set_index('id')
-    network = gpd.read_file(args.pkg, layer='network').set_index('id')
+    # nexi = gpd.read_file(pkg, layer='nexus').set_index('id')
+    fp = gpd.read_file(args.pkg, layer="flowpaths").set_index("id")
+    network = gpd.read_file(args.pkg, layer="network").set_index("id")
     matrix, ts_order = create_matrix(fp, network)
     matrix_to_zarr(matrix, ts_order, args.name, args.path)
 
